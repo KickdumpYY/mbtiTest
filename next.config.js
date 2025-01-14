8c213d5acd740987640028da130e72c651478c83
Yaoyang Huang <huangyaoyang@gzcss.net>
2025-01-14 10:22:57 +0800

    chore: update config for Railway deployment

--- next.config.js
+++ next.config.js
@@ -4,7 +4,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
     domains: ['kickdumpyy.github.io'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.railway.app',
+      },
+    ],
   },
+  output: 'standalone',
 }
 
 module.exports = nextConfig 
